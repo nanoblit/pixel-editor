cfa1b140647d3db9b7703b2ed4585db6aeef7ff4
nanoblit <kuba9519@gmail.com>
2021-03-16 13:45:48 +0100

    Fix drawing.

--- pixel-editor-front-end/src/components/Editor/DrawingCanvas.tsx
+++ pixel-editor-front-end/src/components/Editor/DrawingCanvas.tsx
@@ -3,11 +3,18 @@ import { CanvasElement, CanvasWrapper } from "./DrawingCanvasStyles";
 
 interface Props {
   resolution: number;
+  colorIdx: number;
 }
 
-const DrawingCanvas: React.FC<Props> = ({ resolution }) => {
+const DrawingCanvas: React.FC<Props> = ({ resolution, colorIdx }) => {
   const pixels = useRef<number[]>([]).current;
-  let isDrawing = useRef(false).current;
+  let [isDrawing, setIsDrawing] = useState(false);
+  const colorMap = useRef<{ [key: number]: string }>({
+    0: "#c7cfa2",
+    1: "#8a966d",
+    2: "#4d513c",
+    3: "#1c1c1c",
+  }).current;
   const [canvas, setCanvas] = useState<HTMLCanvasElement>();
   const [ctx, setCtx] = useState<CanvasRenderingContext2D>();
   const canvasRef = useCallback((node: HTMLCanvasElement) => {
@@ -21,21 +28,20 @@ const DrawingCanvas: React.FC<Props> = ({ resolution }) => {
     }
   }, []);
 
-  const drawPixels = (canvas: HTMLCanvasElement) => {
+  const drawPixels = () => {
     for (let i = 0; i < pixels.length; i++) {
       const x = i % resolution;
       const y = Math.floor(i / resolution);
 
-      drawPixel(canvas, pixels[i], x, y);
+      drawPixel(pixels[i], x, y);
     }
   };
 
-  const drawPixel = (
-    canvas: HTMLCanvasElement,
-    color: number,
-    x: number,
-    y: number
-  ) => {
+  const drawPixel = (color: number, x: number, y: number) => {
+    if (!canvas) {
+      return;
+    }
+
     const ctx = canvas.getContext("2d");
     const ppd = canvas.width / resolution;
 
@@ -43,64 +49,72 @@ const DrawingCanvas: React.FC<Props> = ({ resolution }) => {
       return;
     }
 
-    ctx.fillStyle = color === 0 ? "black" : "blue";
+    ctx.fillStyle = colorMap[color];
     const canvasX = Math.floor(x * ppd);
     const canvasY = Math.floor(y * ppd);
 
     ctx.fillRect(canvasX, canvasY, Math.ceil(ppd), Math.ceil(ppd));
   };
 
-  const handleLoad = () => {
+  const setCanvasPixelSize = () => {
     if (!canvas) {
       return;
     }
+
     const rect = canvas.getBoundingClientRect();
     canvas.width = rect.width;
     canvas.height = rect.height;
-
-    drawPixels(canvas);
   };
 
-  const handleResize = () => {
+  const handleLoadAndResize = () => {
+    setCanvasPixelSize();
+    drawPixels();
+  };
+
+  const setAndDrawPixel = (clientX: number, clientY: number) => {
     if (!canvas) {
       return;
     }
-    const rect = canvas.getBoundingClientRect();
-    canvas.width = rect.width;
-    canvas.height = rect.height;
-    drawPixels(canvas);
-  };
 
-  const handleCanvasMouseDown = (e: MouseEvent) => {
-    if (e.button === 0) {
-      isDrawing = true;
-    }
-  };
-
-  const handleCanvasMouseMove = (e: MouseEvent) => {
-    if (!canvas || !ctx || !isDrawing) {
-      return;
-    }
     const ppd = canvas.width / resolution;
     const rect = canvas.getBoundingClientRect();
 
-    const x = Math.floor((e.clientX - rect.left) / ppd);
-    const y = Math.floor((e.clientY - rect.top) / ppd);
+    const x = Math.floor((clientX - rect.left) / ppd);
+    const y = Math.floor((clientY - rect.top) / ppd);
 
-    pixels[y * resolution + x] = 1;
-    drawPixel(canvas, 1, x, y);
+    // set this pixel to currently selected color
+    pixels[y * resolution + x] = colorIdx;
+    drawPixel(colorIdx, x, y);
+  };
+
+  const handleCanvasMouseDown = (
+    e: React.MouseEvent<HTMLCanvasElement, MouseEvent>
+  ) => {
+    if (e.button === 0) {
+      setAndDrawPixel(e.clientX, e.clientY);
+      setIsDrawing(() => true);
+    }
+  };
+
+  const handleCanvasMouseMove = (
+    e: React.MouseEvent<HTMLCanvasElement, MouseEvent>
+  ) => {
+    if (!isDrawing) {
+      return;
+    }
+    setAndDrawPixel(e.clientX, e.clientY);
   };
 
   useEffect(() => {
     for (let i = 0; i < resolution * resolution; i++) {
-      pixels.push(0);
+      pixels.push(3);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleCanvasMouseUp = (e: MouseEvent) => {
     if (e.button === 0) {
-      isDrawing = false;
+      setIsDrawing(() => false);
     }
   };
 
@@ -108,17 +122,13 @@ const DrawingCanvas: React.FC<Props> = ({ resolution }) => {
     if (!canvas) {
       return;
     }
-    window.addEventListener("load", handleLoad);
-    window.addEventListener("resize", handleResize);
-    canvas.addEventListener("mousedown", handleCanvasMouseDown);
-    canvas.addEventListener("mousemove", handleCanvasMouseMove);
+    window.addEventListener("load", handleLoadAndResize);
+    window.addEventListener("resize", handleLoadAndResize);
     window.addEventListener("mouseup", handleCanvasMouseUp);
 
     return () => {
-      window.removeEventListener("load", handleLoad);
-      window.removeEventListener("resize", handleResize);
-      canvas.removeEventListener("mousedown", handleCanvasMouseDown);
-      canvas.removeEventListener("mousemove", handleCanvasMouseMove);
+      window.removeEventListener("load", handleLoadAndResize);
+      window.removeEventListener("resize", handleLoadAndResize);
       window.removeEventListener("mouseup", handleCanvasMouseUp);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -126,7 +136,11 @@ const DrawingCanvas: React.FC<Props> = ({ resolution }) => {
 
   return (
     <CanvasWrapper>
-      <CanvasElement ref={canvasRef}></CanvasElement>
+      <CanvasElement
+        ref={canvasRef}
+        onMouseDown={handleCanvasMouseDown}
+        onMouseMove={handleCanvasMouseMove}
+      ></CanvasElement>
     </CanvasWrapper>
   );
 };
--- pixel-editor-front-end/src/components/Editor/Editor.tsx
+++ pixel-editor-front-end/src/components/Editor/Editor.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import DrawingCanvas from "./DrawingCanvas";
 
 const Editor = () => {
-  return <DrawingCanvas resolution={32} />;
+  const [drawingColorIdx, setDrawingColorIdx] = useState(0);
+
+  return (
+    <>
+      <DrawingCanvas resolution={32} colorIdx={drawingColorIdx} /> 
+      <button onClick={() => setDrawingColorIdx(() => 0)}>Color 0</button>
+      <button onClick={() => setDrawingColorIdx(() => 1)}>Color 1</button>
+      <button onClick={() => setDrawingColorIdx(() => 2)}>Color 2</button>
+      <button onClick={() => setDrawingColorIdx(() => 3)}>Color 3</button>
+    </>
+  );
 };
 
 export default Editor;
