9716e20d14ab5f820cd058e9f2fe4c4be9b15ccd
nanoblit <kuba9519@gmail.com>
2021-03-16 10:34:28 +0100

    Simple drawing

--- pixel-editor-front-end/src/components/Editor/Editor.tsx
+++ pixel-editor-front-end/src/components/Editor/Editor.tsx
@@ -1,9 +1,20 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { EditorCanvas, EditorWrapper } from "./EditorStyles";
 
 const Editor = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [pixels, setPixels] = useState<number[]>([]);
+  const [canvas, setCanvas] = useState<HTMLCanvasElement>();
+  const [ctx, setCtx] = useState<CanvasRenderingContext2D>();
+  const canvasRef = useCallback((node: HTMLCanvasElement) => {
+    if (!node) {
+      return;
+    }
+    setCanvas(() => node);
+    const context = node.getContext("2d");
+    if (context) {
+      setCtx(() => context);
+    }
+  }, []);
+  const pixels = useRef<number[]>([]).current;
 
   const drawPixels = (canvas: HTMLCanvasElement) => {
     const ctx = canvas.getContext("2d");
@@ -18,61 +29,58 @@ const Editor = () => {
       const y = Math.floor(i / 32);
 
       ctx.fillStyle = pixels[i] === 0 ? "black" : "blue";
-      const canvasX = x * ppd;
-      const canvasY = y * ppd;
-      console.log(canvasX);
-      ctx.fillRect(canvasX, canvasY, ppd, ppd);
+      const canvasX = Math.floor(x * ppd);
+      const canvasY = Math.floor(y * ppd);
+
+      ctx.fillRect(canvasX, canvasY, Math.ceil(ppd), Math.ceil(ppd));
     }
   };
 
   const handleLoad = () => {
+    if (!canvas) {
+      return;
+    }
     const rect = canvas.getBoundingClientRect();
     canvas.width = rect.width;
+    canvas.height = rect.height;
 
     drawPixels(canvas);
   };
 
   const handleResize = () => {
+    if (!canvas) {
+      return;
+    }
     const rect = canvas.getBoundingClientRect();
     canvas.width = rect.width;
-
-    console.log(pixels);
+    canvas.height = rect.height;
     drawPixels(canvas);
   };
 
-  const handleCanvasClick = () => {
-    const ppd = canvas.width / 32;
-    const rect = canvas.getBoundingClientRect();
-    ctx.fillStyle = "blue";
-    const x = Math.round((e.clientX - rect.left) / ppd);
-    const y = Math.round((e.clientY - rect.top) / ppd);
-    console.log(`x: ${x}, y: ${y}`);
-
-    // setPixels((prev) => {
-    //   prev
-    // })
-
-    // drawPixels(canvas);
-  };
-
-  useEffect(() => {
-    setPixels(() => {
-      const newPixels: number[] = [];
-      for (let i = 0; i < 32 * 32; i++) {
-        newPixels.push(0);
-      }
-      return newPixels;
-    });
-  }, []);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas?.getContext("2d");
-
+  const handleCanvasClick = (e: MouseEvent) => {
     if (!canvas || !ctx) {
       return;
     }
+    const ppd = canvas.width / 32;
+    const rect = canvas.getBoundingClientRect();
 
+    const x = Math.floor((e.clientX - rect.left) / ppd);
+    const y = Math.floor((e.clientY - rect.top) / ppd);
+
+    pixels[y * 32 + x] = 2;
+    drawPixels(canvas);
+  };
+
+  useEffect(() => {
+    for (let i = 0; i < 32 * 32; i++) {
+      pixels.push(0);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!canvas) {
+      return;
+    }
     window.addEventListener("load", handleLoad);
     window.addEventListener("resize", handleResize);
     canvas.addEventListener("click", handleCanvasClick);
@@ -82,9 +90,9 @@ const Editor = () => {
       window.removeEventListener("resize", handleResize);
       canvas.removeEventListener("click", handleCanvasClick);
     };
-  }, [pixels]);
+  }, [canvas, pixels]);
 
-  useEffect(() => console.log(pixels), [pixels]);
+  // useEffect(() => console.log(pixels), [pixels]);
 
   return (
     <EditorWrapper>
